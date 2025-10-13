99e4de3b4c9965552aaa1f3531fa776672134803
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-10-13 11:02:13 +0530

    init-3

--- INDEX_Files/src/Props-Drilling/Ex3_Extended/App.jsx
+++ INDEX_Files/src/Props-Drilling/Ex3_Extended/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { fetchProducts } from './API/productApi';
 
 const App = () => {
@@ -9,9 +9,15 @@ const App = () => {
     useEffect(()=>{
         fetchProducts().then(setProducts);
     },[]);
+
+    const addToCart = (product) => {
+        setCart(prev=>{
+            const existing = prev.find(item => )
+        })
+    }
   return (
     <div>
-    
+        
     </div>
   )
 }
