6c2f244a40855fbca481e3e8eff685aec6941564
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-10-31 00:19:44 +0530

    Initiated Example 3 Router Dom

---
+++ INDEX_Files/src/React_Router_DOM/Ex3/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Product from './pages/Product'
+import Items from './pages/Items'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black text-white '>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/product/items' element={<Items />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App
---
+++ INDEX_Files/src/React_Router_DOM/Ex3/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='absolute bottom-0 w-full bg-cyan-900 flex py-4 items-center justify-center'>
+      <h2 className='font-bold text-lg'>This is Footer.</h2>
+    </div>
+  )
+}
+
+export default Footer
