b7ef8291df8c918756fe75dee38e1cc8d49357a0
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-08 02:33:28 +0530

    Completed Ex1 : Throttling..git add .!

--- INDEX_Files/src/Raw-JavaScript/Throttling/Ex1/app.jsx
+++ INDEX_Files/src/Raw-JavaScript/Throttling/Ex1/app.jsx
@@ -1,24 +1,18 @@
 import { throttling } from './index'
-
 function printFunction(){
     console.log("Hello, ");
 }
-
 const App = () => {
-
     let throttlePrint = throttling(printFunction,1000);
-    
     return (
         <div>
             <input 
                 type="text"
                 placeholder='type anything...!'
                 onInput={(e)=> throttlePrint(e.target.value)}
-                className='border-4 border-blue-600 rounded-2xl animate-bounce'
+                className='border-4 border-blue-600 rounded-2xl animate-pulse'
              />
-        
         </div>
     )
 }
-
 export default App
--- INDEX_Files/src/Raw-JavaScript/Throttling/Ex1/index.js
+++ INDEX_Files/src/Raw-JavaScript/Throttling/Ex1/index.js
@@ -1,10 +1,10 @@
 export function throttling(fnc,delay) {
-    let timer;
+    let timer = 0;
 
     return function(...args) {
         let now = Date.now();
-        if (now - timer >=delay){
-            timer=now;
+        if (now - timer >= delay) {
+            timer = now;
             fnc(...args);
         }
 
