aba21f82de6675bcf5c50a17d5a54b45c1884e3a
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-09 14:50:05 +0530

    added major chnages in ex2 code splitter example

--- INDEX_Files/src/Raw-JavaScript/CodeSplitting/Ex2/App.jsx
+++ INDEX_Files/src/Raw-JavaScript/CodeSplitting/Ex2/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+
+
 
 const App = () => {
+
+  // const btn = document.querySelector("button");
+
+  async function loadHeavyFunction(){
+    let heavy = await import('./heavy.js');
+    console.log(heavy);
+  }
+
   return (
     <div>
-      
+      <h1>Code Splitting with Dynamic import.</h1>
+      <button onClick={loadHeavyFunction} >Click to Load Heavy Module.</button>
     </div>
   )
 }
---
+++ INDEX_Files/src/Raw-JavaScript/CodeSplitting/Ex2/heavy.js
@@ -0,0 +1,4 @@
+export default function veryHeavy(){
+    console.log("Giant feature added ..!");
+    alert("heavy featrure loading completed !");
+};
