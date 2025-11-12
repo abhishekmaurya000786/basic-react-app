ba4b2c4d758a73852c409d3e87ebb7a800d7280b
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-12 23:59:55 +0530

    COMPLETED EX5 CODE SOLITTING

---
+++ INDEX_Files/src/Raw-JavaScript/CodeSplitting/Ex5/Components/Dashboard.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Dashboard() {
+  console.log("📦 Dashboard component loaded!");
+  return <h2 className="text-xl text-purple-500">Dashboard Page Content</h2>;
+}
