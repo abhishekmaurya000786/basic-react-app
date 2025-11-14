f2874b3b6e1e36dee145b8100d1c8f93c98e0e54
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-14 23:10:08 +0530

    added major changes..!

--- INDEX_Files/src/Redux/Ex1/App.jsx
+++ INDEX_Files/src/Redux/Ex1/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Counter from './Components/Counter';
+
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default App
+return (
+<div className="h-screen flex items-center justify-center bg-black">
+<Counter />
+</div>
+);
+};
