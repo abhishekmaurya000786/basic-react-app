ac32bb9a95a15fe768bdad70a7a6cb4f3e3934f7
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-17 23:27:46 +0530

    added minor changes in Ex5 redux

--- INDEX_Files/src/Redux/Ex5/main.jsx
+++ INDEX_Files/src/Redux/Ex5/main.jsx
@@ -1,8 +1,11 @@
-import ReactDOM from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import App from "./App";
+import store from './Store/store'
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
