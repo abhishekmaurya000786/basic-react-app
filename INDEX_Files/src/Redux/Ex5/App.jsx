0a7925121188c584ad4a4662c4783b544145c84f
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-21 23:56:25 +0530

    Added major changes Ex6 REDUX

--- INDEX_Files/src/Redux/Ex5/App.jsx
+++ INDEX_Files/src/Redux/Ex5/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import {increment, decrement} from './Features/Counter/counterSlice'
+import {increment, decrement,reset, incrementByAmount} from './Features/Counter/counterSlice'
 
 const App = () => {
 
@@ -16,6 +16,14 @@ const App = () => {
     dispatch(decrement());
   };
 
+  function handleReset(){
+    dispatch(reset());
+  };
+
+  function handleSetAmount(){
+    dispatch(incrementByAmount)
+  }
+
   return (
     <div className='flex flex-col items-center justify-center'>
       <button 
@@ -24,6 +32,9 @@ const App = () => {
       >+</button>
       <h1 className='text-white'>Count : ${count}</h1>
       <button onClick={handleDecrement} className='btn'>-</button>
+      <button onClick={handleReset} className='btn'>RESET</button>
+      <input type="number"  />
+      <button onClick={handleSetAmount()}></button>
     </div>
   )
 }
