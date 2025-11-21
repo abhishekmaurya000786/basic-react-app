21eff02fcdb0805bd4721975104b151077495c0c
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-21 22:13:14 +0530

    COMPLETED Ex5 REDUX

--- INDEX_Files/src/Redux/Ex5/App.jsx
+++ INDEX_Files/src/Redux/Ex5/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {increment, decrement} from './Features/Counter/counterSlice'
 
 const App = () => {
+
+  const count = useSelector((state)=> state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleIncrement(){
+    dispatch(increment());
+  };
+  
+  function handleDecrement(){
+    dispatch(decrement());
+  };
+
   return (
     <div>
-      <h1>Hello Bro</h1>
+      <button onClick={handleIncrement}>+</button>
+      <h1>Count : ${count}</h1>
+      <button onClick={handleDecrement}>-</button>
     </div>
   )
 }
