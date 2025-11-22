b80447717ccc702650e31a399d15da7660643669
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-11-22 13:03:24 +0530

    Finally Completed Ex5 REDUX

--- INDEX_Files/src/Redux/Ex5/App.jsx
+++ INDEX_Files/src/Redux/Ex5/App.jsx
@@ -1,42 +1,57 @@
-import React from 'react';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import {increment, decrement,reset, incrementByAmount} from './Features/Counter/counterSlice'
+import React,{useState} from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import {increment,decrement,reset,incrementByAmount} from "./Features/Counter/counterSlice";
 
 const App = () => {
+  const [amount, setAmount] = useState(0);
 
-  const count = useSelector((state)=> state.counter.value);
+  const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
-  function handleIncrement(){
+  function handleIncrement() {
     dispatch(increment());
-  };
-  
-  function handleDecrement(){
+  }
+
+  function handleDecrement() {
     dispatch(decrement());
-  };
+  }
 
-  function handleReset(){
+  function handleReset() {
     dispatch(reset());
-  };
+  }
 
-  function handleSetAmount(){
-    dispatch(incrementByAmount)
+  function handleIncAmount() {
+    dispatch(incrementByAmount(amount));
   }
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <button 
-        onClick={handleIncrement}
-        className='btn'
-      >+</button>
-      <h1 className='text-white'>Count : ${count}</h1>
-      <button onClick={handleDecrement} className='btn'>-</button>
-      <button onClick={handleReset} className='btn'>RESET</button>
-      <input type="number"  />
-      <button onClick={handleSetAmount()}></button>
+    <div className="flex flex-col items-center justify-center">
+      <button onClick={handleIncrement} className="btn">
+        +
+      </button>
+      <h1 className="text-white">Count : ${count}</h1>
+      <button onClick={handleDecrement} className="btn">
+        -
+      </button>
+      <button onClick={handleReset} className="btn">
+        RESET
+      </button>
+      <div>
+        <input type="number" className="btn"
+        onChange={(e) => setAmount(e.target.value)}
+        />
+        <button
+          onClick={handleIncAmount}
+          value={amount}
+          placeholder="Enter Amount"
+          className="btn"
+        >
+          Increment by Amount
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
