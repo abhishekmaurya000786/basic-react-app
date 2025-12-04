9ea05076fff3d526caffc8e9413008d794034595
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-04 19:39:36 +0530

    added changes in  Actions.. Ex7 REDUX

---
+++ INDEX_Files/src/Redux/Ex7/Actions/reset.js
@@ -0,0 +1,12 @@
+import { createAction } from "@reduxjs/toolkit";
+
+export const resetApp = createAction("app/reset");
+
+
+
+
+//    (Multiple other actions that could be defined here)
+// export const logout = createAction("user/logout");
+// export const hydrate = createAction("app/hydrate");
+// export const userSwitched = createAction("user/switched");
+// export const clearCache = createAction("cache/clear");
