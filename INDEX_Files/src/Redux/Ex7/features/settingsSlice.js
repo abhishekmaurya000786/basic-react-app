f5be34655f48a762e9da595b4a3d1a9d422b1c99
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-06 02:25:01 +0530

    added addCase in settingSlice.js extraReducers in Ex7 REDUX

--- INDEX_Files/src/Redux/Ex7/features/settingsSlice.js
+++ INDEX_Files/src/Redux/Ex7/features/settingsSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUser } from "./userSlice";
+import { resetApp } from "../Actions/globalActions";
 
 
 
@@ -7,5 +9,17 @@ export const settingSlice = createSlice({
     initialState: {
         theme: "light",
         lastLoadedUser: null,
+    },
+    reducers:{},
+
+    extraReducers: (builder)=> {
+        builder
+        .addCase(fetchUser.fulfilled,(state,action)=>{
+            state.lastLoadedUser = action.payload.name;
+        })
+        .addCase(resetApp, (state)=>{
+            state.theme = "light";
+            state.lastLoadedUser = "";
+        });
     }
 });
