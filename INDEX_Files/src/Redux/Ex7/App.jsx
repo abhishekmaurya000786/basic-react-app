fb9877f905457e4f116f21fa073489c1caabb94c
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-06 23:58:11 +0530

    Initated setup in app.jsx in Ex7 REDUX

--- INDEX_Files/src/Redux/Ex7/App.jsx
+++ INDEX_Files/src/Redux/Ex7/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const App = () => {
+
+    const dispatch = useDispatch();
+    const user = useSelector(state=>state.user);
+    const settings = useSelector(state=>state.settings)
+
   return (
     <div>
       
