aa419f76d3db59477eae9513110a86a3449fff2c
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-07 03:14:48 +0530

    Added major components in application in app.jsx in Ex7 REDUX

--- INDEX_Files/src/Redux/Ex7/App.jsx
+++ INDEX_Files/src/Redux/Ex7/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUser, setMessage } from "./features/userSlice";
 
 const App = () => {
-
-    const dispatch = useDispatch();
-    const user = useSelector(state=>state.user);
-    const settings = useSelector(state=>state.settings)
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const settings = useSelector((state) => state.settings);
 
   return (
-    <div>
-      
+    <div className="">
+      <h1>Redux learning app.</h1>
+      <button onClick={() => dispatch(fetchUser())}>Get user.</button>
+      <button onClick={() => dispatch(setMessage("custom message set."))}>
+        Set message.
+      </button>
+      <button onClick={() => dispatch()}>Reset App.</button>
+      <button onClick={() => dispatch()}>Force Layout.</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
