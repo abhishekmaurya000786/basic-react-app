5d460aefcc448cd6eb9ca55050c995ebbe747611
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-12 23:56:17 +0530

    added few changes in taskSlice in ex9 redux

--- INDEX_Files/src/Redux/Ex9/features/taskSlice.js
+++ INDEX_Files/src/Redux/Ex9/features/taskSlice.js
@@ -1,4 +1,19 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { resetAll } from "../Actions/actoin";
 
-export const fetchTasks = createAsyncThunk();
+export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=3');
+    return await res.json();
+});
+
+const taskSlice = createSlice({
+    name: 'tasks',
+    initialState: {
+        items: [],
+        status:"idle",
+    },
+    reducers: {
+        addTaskOptimistic: (state, action) => {
+            state.items.push(action.payload);
+        }
+}); 
