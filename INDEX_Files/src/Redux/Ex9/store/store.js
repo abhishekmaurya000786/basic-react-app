7bbd1fccd84969344b253c31f8138642860df255
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-13 21:39:23 +0530

    Added and configured Reducers inside the store in Ex9 REDUX

--- INDEX_Files/src/Redux/Ex9/store/store.js
+++ INDEX_Files/src/Redux/Ex9/store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-
+import {taskReducer} from '../features/taskSlice'
+import {errorReducer} from '../features/errorSliceManager';
+import {profileReducer} from '../features/taskSlice';
 
 export const store = configureStore({
     reducer : {
-        
+        tasks : taskReducer,
+        errors : errorReducer,
+        profile : profileReducer,
     }
 });
