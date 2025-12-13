2abbbc300939c6b1a73b8a86b5bfc94e9a01ae7d
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-13 18:52:30 +0530

    Added taskSlice in Ex9 REDUX

--- INDEX_Files/src/Redux/Ex9/features/taskSlice.js
+++ INDEX_Files/src/Redux/Ex9/features/taskSlice.js
@@ -14,6 +14,32 @@ const taskSlice = createSlice({
     },
     reducers: {
         addTaskOptimistic: (state, action) => {
-            state.items.push(action.payload);
+            state.items.push({
+                id: Date.now(),
+                title: action.payload,
+                optimiidtic: true,
+            });
         }
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(fetchTasks.pending, (state)=>{
+            state.status = "pending";
+
+        })
+        .addCase(fetchTasks.fulfilled, (state,action)=>{
+            state.status = "done";
+            state.items = action.payload;
+        })
+        .addCase(fetchTasks.rejected, (state)=>{
+            state.status = "error";
+        })
+        .addCase(resetAll, (state)=>{
+            state.items = [];
+            state.status = "idle";
+        });
+    }
 }); 
+
+export const { addTaskOptimistic } = taskSlice.actions;
+
+export default taskSlice.reducer;
