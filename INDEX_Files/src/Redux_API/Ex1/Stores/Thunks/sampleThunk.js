9dc69f5aa8f362e0d4e2e11fba1a9437621da511
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-24 23:58:37 +0530

    Added Store and configured it in Ex1 REDUX API.

--- INDEX_Files/src/Redux_API/Ex1/Stores/Thunks/sampleThunk.js
+++ INDEX_Files/src/Redux_API/Ex1/Stores/Thunks/sampleThunk.js
@@ -1,8 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const sampleThunk = createAsyncThunk({
+export const sampleThunk = createAsyncThunk(
     "sample/fetchSomething", async () => {
         const res = fetch("httpshttps://dummyjson.com/products/1");
         return res.json();
     }
-});
+);
--- INDEX_Files/src/Redux_API/Ex1/Stores/store.js
+++ INDEX_Files/src/Redux_API/Ex1/Stores/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import counterReducer from '../Stores/Slices/counterSlice'
+import {productApi} from '../Stores/API/productApi'
 
 export const store = configureStore({
     reducer : {
+        counter: counterReducer,
+
+        [productApi.reducerpath]:productApi.reducer,
 
     }
 });
