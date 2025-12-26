603c814bc9f1216490a8fec02b6a44b9f86b516d
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-26 19:36:49 +0530

    Cnfigured App.jsx in Ex1 REDUX API.

--- INDEX_Files/src/Redux_API/Ex1/App.jsx
+++ INDEX_Files/src/Redux_API/Ex1/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Product from './Components/Product'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen bg-gray-100'>
+      <h1 className='text-center text-2xl font-bold p-6'>REDUX API Example</h1>
+      <Product />
     </div>
   )
 }
