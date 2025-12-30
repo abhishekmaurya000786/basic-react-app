cfc13309da3f2e7927bc253eb56e4ce4747e316c
dev-Abhishek <abhishekmaurya8401@gmail.com>
2025-12-30 21:23:44 +0530

    Completed Ex1 made a basic server.

--- INDEX_Files/src/Nodes_Server/Ex1/app.js
+++ INDEX_Files/src/Nodes_Server/Ex1/app.js
@@ -1,1 +1,4 @@
-console.log("Hello world !")
+// const math = require("./Modules"); 
+const {add, subtract} = require("./Modules/maths");
+console.log("Sum of a and b is ", add(5,2));
+console.log("Difference of a and b is ", subtract(5,2));
