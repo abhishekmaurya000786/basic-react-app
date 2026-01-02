89043c31f80f729b1edc6bb56074a55744dbadc1
dev-Abhishek <abhishekmaurya8401@gmail.com>
2026-01-02 21:48:43 +0530

    Completed Ex2 Node Server.

--- INDEX_Files/src/Node_SERVER/Ex2/app.js
+++ INDEX_Files/src/Node_SERVER/Ex2/app.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.get('/',function(req,res){      // this function is always a request handler
     res.send("Hello World ABHISHEK jiiii    ok ji");
-})
+});
+
+app.get('/profile',function(req,res){      // this function is always a request handler
+    res.send("ABHISHEK OKAY PROFILE PAGE");
+});
 
 app.listen(3000);
 // app.listen(port, hostname, backlog)
