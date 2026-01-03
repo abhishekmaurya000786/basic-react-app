1ffb46497adc43a79a8dfacfd4e49dd934909af3
dev-Abhishek <abhishekmaurya8401@gmail.com>
2026-01-03 23:49:06 +0530

    Completed Ex4 Node Server.

---
+++ INDEX_Files/src/Node_SERVER/Ex4/App.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const {createServer} = require("http");
+const { data } = require("autoprefixer");
+
+const fs = ('fs');
+
+const server = createServer((req,res)=>{
+    // This will handle only root url only !
+    if (req.url == '/'){
+
+        // reading file uasing callback API
+
+        // fs.readFile('index.html', "utf8", (err,data)=>{
+        fs.readFile(path.join(__dirname,'index.html'), "utf8", (err,data)=>{
+            if (err) {
+                res.writeHead(500, {"Content-Type":"text/html"});
+                res.end("Internal Error..");
+                return;
+            }
+
+            // if file reads succeeds
+            res.writeHead(200, {"Content-Type" : "text/html"});
+            res.end(data);
+        });
+    } else {
+        // Handles Unknown routes !
+        res.writeHead(500, {"content-type" : "text/plain"});
+        res.end("Page not Found !");
+    }
+});
+
+server.listen(3000, ()=>{
+    console.log("Server running at http://localhost:3000");
+});
