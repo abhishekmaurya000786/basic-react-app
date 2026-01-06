95def0e65eeb514cd479ed85c624cd6c11954b84
dev-Abhishek <abhishekmaurya8401@gmail.com>
2026-01-06 17:20:55 +0530

    COMPLETED Ex6 Node Server

--- INDEX_Files/src/Node_SERVER/Ex6/app.js
+++ INDEX_Files/src/Node_SERVER/Ex6/app.js
@@ -1,3 +1,23 @@
 const http = require("http");
 const fs = require("fs").promises;
-const path = require("path");
+// const path = require("path");
+
+
+const server = http.createServer((req,res)=>{
+    const log = `${Date.now()} : ${req.url} New Request Recieved !\n`;
+
+    fs.appendFile('./Data.txt', log,(err,data)=>{
+        switch(req.url){
+            case '/':
+                res.end("Homepage"); break;
+            case '/about':
+                res.end("This is about page "); break;
+            default:
+                res.end("404 Not Found !");
+        }
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log("Yes, Listening..! at http://localhost:3000")
+})
