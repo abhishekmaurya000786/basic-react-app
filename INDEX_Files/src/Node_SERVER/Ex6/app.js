6ffd3abbbd49aa85f52403900051948a3b35fbf9
dev-Abhishek <abhishekmaurya8401@gmail.com>
2026-01-06 22:51:20 +0530

    COMPLETED Ex6 Node Server..

--- INDEX_Files/src/Node_SERVER/Ex6/app.js
+++ INDEX_Files/src/Node_SERVER/Ex6/app.js
@@ -1,17 +1,27 @@
 const http = require("http");
-const fs = require("fs").promises;
+const fs = require("fs");
+const url = require("url");
 // const path = require("path");
 
 
 const server = http.createServer((req,res)=>{
     const log = `${Date.now()} : ${req.url} New Request Recieved !\n`;
 
+    const myUrl = url.parse(req.url);
+    console.log(myUrl);
+
+    if (req.url === '/favicon.ico') return res.end();
+
     fs.appendFile('./Data.txt', log,(err,data)=>{
+        // switch(myUrl.pathname){
         switch(req.url){
             case '/':
                 res.end("Homepage"); break;
+
+                // http://localhost:3000/about?myname=abhishek&userid=1&search=dog search it on chrome..!
             case '/about':
-                res.end("This is about page "); break;
+                // const username = myUrl.query.pathname;
+                res.end("Hi"); break; // ${username}
             default:
                 res.end("404 Not Found !");
         }
